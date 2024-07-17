12b93cbdfef843735d8b66d9545a0905c29008bc
Edenilson <42663184+edenilsonpineda@users.noreply.github.com>
2024-07-17 15:41:46 -0600

    Initial commit

---
+++ src/main/java/com/example/fake_store_cloud_config/FakeStoreCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.fake_store_cloud_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeStoreCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fake_store_cloud_config/FakeStoreCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fake_store_cloud_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
