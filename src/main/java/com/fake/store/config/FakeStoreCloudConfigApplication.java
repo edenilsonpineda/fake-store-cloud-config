9db9c38ecc1dcca5883600c44383da2fb7f8fe24
Edenilson <42663184+edenilsonpineda@users.noreply.github.com>
2024-07-25 16:27:49 -0600

    chore: rename packages

--- src/main/java/com/fake/store/config/FakeStoreCloudConfigApplication.java
+++ src/main/java/com/fake/store/config/FakeStoreCloudConfigApplication.java
@@ -1,13 +1,13 @@
-package com.example.fake_store_cloud_config;
+package com.fake.store.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class FakeStoreCloudConfigApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FakeStoreCloudConfigApplication.class, args);
 	}
-
 }
